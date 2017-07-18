5b6afa693f5a0eac1da842a43f4da00ec0fc1ee3
Alberto Cortés <alcortesm@gmail.com>
2017-07-18 20:26:03 +0200

    improve documentation

--- queue.go
+++ queue.go
@@ -1,13 +1,13 @@
 /*
-Package queue contains an interface and implementations for queues of
-elements of the empty interface type.
+Package queue contains an interface and some implementations for queues
+of elements of the empty interface type.
 */
 package queue
 
 import "errors"
 
-// A Queue represents an interface for a collection of elements of the
-// empty interface type with queue access semantics.
+// A Queue interface represents a queue of elements of the empty
+// interface type.
 //
 // This interface makes no assumtions about the thread safety of its
 // implementations.
@@ -17,23 +17,26 @@ type Queue interface {
 	// IsEmpty returns if the queue has no elements.
 	IsEmpty() bool
 	// Enqueue tries to add the given element at the back of the queue
-	// and returns a nil error on success or an error on failure.
+	// and returns a nil error on success or an error on failure.  The
+	// error will be ErrFull if the queue ran out of resources to
+	// accommodate the given element.
 	Enqueue(interface{}) error
 	// Head returns the first element in the queue and a nil error on
-	// success.  It returns nil and an error on failure.  The error will
-	// be ErrEmpty if the queue was originally empty.  Head is idempotent.
+	// success.  On failuer, it returns nil and an error.  The error
+	// will be ErrEmpty if the queue was originally empty.  Head is
+	// idempotent.
 	Head() (interface{}, error)
 	// Dequeue extracts and returns the first element in the queue and a
-	// nil error on success.  It returns nil and an error on failure.
+	// nil error on success.  On failure, it returns nil and an error.
 	// The error will be ErrEmpty if the queue was originally empty.
 	Dequeue() (interface{}, error)
 }
 
 var (
 	// ErrEmpty is returned by the Head and Dequeue methods if the queue
-	// was originally empty.
+	// is empty.
 	ErrEmpty = errors.New("empty queue")
-	// ErrFull is returned by Enqueue when there in not enough resources
-	// to accommodate the given element.
+	// ErrFull is returned by the Enqueue method when there is not
+	// enough resources to accommodate the given element.
 	ErrFull = errors.New("full queue")
 )
